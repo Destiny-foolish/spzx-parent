955ca74f6b1f71bdd918460f5c2db3c5c67e43e6
Destiny-foolish <63221305+Destiny-foolish@users.noreply.github.com>
2023-11-15 12:22:18 +0800

    11.15导入功能有问题，图片上传待修改，暂时提交

---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/BrandController.java
@@ -0,0 +1,35 @@
+package com.atguigu.spzx.manager.controller;
+
+import com.atguigu.spzx.manager.service.BrandService;
+import com.atguigu.spzx.model.entity.product.Brand;
+import com.atguigu.spzx.model.vo.common.Result;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value="/admin/product/brand")
+public class BrandController {
+
+    @Autowired
+    private BrandService brandService;
+
+    @GetMapping("/{page}/{limit}")
+    public Result<PageInfo<Brand>> findByPage(@PathVariable Integer page, @PathVariable Integer limit) {
+        PageInfo<Brand> pageInfo = brandService.findByPage(page, limit);
+        return Result.build(pageInfo , ResultCodeEnum.SUCCESS) ;
+    }
+
+    @PostMapping("save")
+    public Result save(@RequestBody Brand brand) {
+        brandService.save(brand);
+        return Result.build(null , ResultCodeEnum.SUCCESS) ;
+    }
+
+    @PutMapping("updateById")
+    public Result updateById(@RequestBody Brand brand) {
+        brandService.updateById(brand);
+        return Result.build(null , ResultCodeEnum.SUCCESS) ;
+    }
+}
---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/mapper/BrandMapper.java
@@ -0,0 +1,15 @@
+package com.atguigu.spzx.manager.mapper;
+
+import com.atguigu.spzx.model.entity.product.Brand;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BrandMapper {
+    List<Brand> findByPage();
+
+    void save(Brand brand);
+
+    void updateById(Brand brand);
+}
